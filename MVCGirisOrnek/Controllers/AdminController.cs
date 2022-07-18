b8e3861b609c0c35f1a9f4a12a177e235ba81b81
nesendal <nagihanesendal89@gmail.com>
2022-07-18 20:09:49 +0300

    Mvc Admin Panel Ornek
    * MC-1015 -ders ekleme özelliği tanımlandı.

---
+++ MVCGirisOrnek/Controllers/AdminController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCGirisOrnek.Models;
+using System.Linq;
+
+namespace MVCGirisOrnek.Controllers
+{
+    public class AdminController : Controller
+    {
+        UyeDBContext veritabani = new UyeDBContext();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //uyeEkle olsun
+        //geriye string dönen bir metot yaazalım.
+        //public string UyeEkle()
+        //{
+        //    return "Uye eklenecek";
+        //}
+
+        //uyeEkle sayfasını gözümüze açması için, o sayfayı yüklemesi için
+        public IActionResult UyeEkle()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        public IActionResult UyeEkle(Uye uye)
+        {
+            veritabani.Uyes.Add(uye);
+            veritabani.SaveChanges();
+            return View();
+        }
+
+        public IActionResult Uyeler()
+        {
+            var liste = veritabani.Uyes.ToList();
+            return View(liste);
+        }
+
+        //uye düzenleme
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var uye = veritabani.Uyes.Find(id);
+            if (uye == null)
+            {
+                return NotFound();
+            }
+            return View(uye);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id,Uye uye)
+        {
+            if (id != uye.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                veritabani.Update(uye);
+                veritabani.SaveChanges();
+
+                return RedirectToAction(nameof(Uyeler)); 
+                //düzenleme işlemi sonrası uyeler listesine ddönsün diye redirecttoAction diyip uyeler sayfasına gönderilir.
+            }
+            return View(uye);
+        }
+
+
+        // GET: Uyes/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var uye =  veritabani.Uyes.FirstOrDefault(m => m.Id == id);
+            if (uye == null)
+            {
+                return NotFound();
+            }
+
+            return View(uye);
+        }
+
+
+
+        // POST: Uyes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var uye =  veritabani.Uyes.Find(id);            
+            veritabani.Uyes.Remove(uye);
+            veritabani.SaveChanges();
+            return RedirectToAction(nameof(Uyeler));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var uye = veritabani.Uyes.Find(id);
+            if (uye == null)
+            {
+                return NotFound();
+            }
+            return View(uye);
+        }
+    }
+}
---
+++ MVCGirisOrnek/Controllers/UyesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCGirisOrnek.Models;
+
+namespace MVCGirisOrnek.Controllers
+{
+    public class UyesController : Controller
+    {
+        private readonly UyeDBContext _context;
+
+        public UyesController(UyeDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Uyes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Uyes.ToListAsync());
+        }
+
+        // GET: Uyes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var uye = await _context.Uyes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (uye == null)
+            {
+                return NotFound();
+            }
+
+            return View(uye);
+        }
+
+        // GET: Uyes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Uyes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Ad,Soyad,Email,Sifre")] Uye uye)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(uye);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(uye);
+        }
+
+        // GET: Uyes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var uye = await _context.Uyes.FindAsync(id);
+            if (uye == null)
+            {
+                return NotFound();
+            }
+            return View(uye);
+        }
+
+        // POST: Uyes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Ad,Soyad,Email,Sifre")] Uye uye)
+        {
+            if (id != uye.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(uye);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UyeExists(uye.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(uye);
+        }
+
+        // GET: Uyes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var uye = await _context.Uyes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (uye == null)
+            {
+                return NotFound();
+            }
+
+            return View(uye);
+        }
+
+        // POST: Uyes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var uye = await _context.Uyes.FindAsync(id);
+            _context.Uyes.Remove(uye);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UyeExists(int id)
+        {
+            return _context.Uyes.Any(e => e.Id == id);
+        }
+    }
+}
